ca11a4f09c0fce5fb2f1d2eaecea4d535b554e1f
ryy <Riley.Vandever@student.northampton.edu>
2026-02-01 00:48:21 +0000

    Solving Perimeter and Area of a Rectangle

--- main.cpp
+++ main.cpp
@@ -4,6 +4,25 @@ using namespace std;
 int main() {
   const double length = 7.2;
   const double width = 1.3;
+  double end1;
 
+  double area;
+  double perimeter;
 
+//area () 
+area = length * width;
+cout << "length*width = " << area;
+
+//perimeter ()
+perimeter = 2 * (length + width);
+cout << "2*(length+width) = " << perimeter; 
+
+//solving ()
+cout << "Rectangle Properties: " << endl;
+cout << "Length = " << length << endl;
+cout << "Width = " << width << endl; 
+cout << "Area = " << area << endl;
+cout << "Perimeter = " << perimeter << endl;
 }
+
+
